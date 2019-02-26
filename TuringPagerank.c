173217b8b26cf17347f6905c622e0a0a711db1aa
tothantal <43417284+tothantal@users.noreply.github.com>
2019-02-26 12:47:45 +0100

    Update and rename pagerank.c to TuringPagerank.c

--- TuringPagerank.c
+++ TuringPagerank.c
@@ -8,7 +8,7 @@ double tavolsag(double pagerank[], double pagerank_temp[], int db);
 int main(void)
 {
 	double L[4][4] =
-  {
+  	{
 		{0.0, 0.0, 1.0 / 3.0, 0.0},
 		{1.0, 1.0 / 2.0, 1.0 / 3.0, 1.0},
 		{0.0, 1.0 / 2.0, 0.0, 0.0},
@@ -18,28 +18,28 @@ int main(void)
 	double PR[4] = {0.0, 0.0, 0.0, 0.0};
 	double PRv[4] = {1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0};
 
-	for (;;) {
+	for (;;)
+	{
 		for(int i=0; i<4; i++)
-    {
+    		{
 			PR[i] = PRv[i];
 		}
 
 		for (int i=0; i<4; i++)
-    {
+    		{
 			double temp = 0.0;
 
 			for (int j=0; j<4; j++)
-      {
+      			{
 				temp += L[i][j] * PR[j];
 				PRv[i] = temp;
 			}
 		}
 
 		if(tavolsag(PR, PRv, 4) < 0.000001)
-    {
+    		{
 			break;
 		}
-	}
 
 	kiir(PR, 4);
 
@@ -48,8 +48,8 @@ int main(void)
 
 void kiir (double tomb[], int db)
 {
-  for (int i=0; i<db; i++)
-  {
+	for (int i=0; i<db; i++)
+	{
 		printf("PageRank [%d]: %lf\n", i, tomb[i]);
 	}
 }
@@ -59,7 +59,7 @@ double tavolsag(double pagerank[], double pagerank_temp[], int db)
 	double dist = 0.0;
 
 	for(int i=0; i<db; i++)
-  {
+	{
 		dist += (pagerank[i] - pagerank_temp[i]) * (pagerank[i] - pagerank_temp[i]);
 	}
 
